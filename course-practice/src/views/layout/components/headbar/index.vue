6cc0f0e1678954e82f78073b8678a99056dd25a1
haiboshi2003 <134484555+haiboshi2003@users.noreply.github.com>
2024-06-25 10:33:48 +0800

    first

---
+++ course-practice/src/views/layout/components/headbar/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="header">
+    <div class="img">
+      <img src="Tj.png" alt="Header Image" />
+    </div>
+    <div class="hhh">
+      <p>同济大学软件测试</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeadBar",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    changeCollapse() {
+      this.$emit("changeCollapse", 0);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.header {
+  background-color: rgb(242, 242, 242);
+
+  display: flex;
+  flex-direction: row;
+
+  height: 65px;
+}
+.hhh {
+  // background-color: aqua;
+  // padding: 0px 0 0 0;
+  width: 210px;
+  height: 100px;
+  margin-bottom: 20px;
+  margin-top: 0px;
+  // border: 1px solid blue;
+}
+
+.img {
+  // background-color: black;
+  margin-top: 2px;
+  padding: 6px 10px 0 40px; /* 设置图像框的内边距 */
+  // border: 1px solid blue;
+  width: 70px;
+  max-width: 120px;
+  height: 50px;
+  max-height: 50px;
+  // border: 1px solid blue;
+}
+
+.img img {
+  max-width: 100%; /* 图片的最大宽度不超过父元素的宽度 */
+  max-height: 100%; /* 图片的最大高度不超过父元素的高度 */
+  display: block; /* 让图片以块级元素显示 */
+}
+p {
+  font-style: italic;
+  font-family: "宋体", SimSun, sans-serif;
+  font-size: 23px;
+  color: rgb(12, 12, 12);
+}
+</style>
---
+++ course-practice/src/views/thirteen/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <el-card shadow="hover" class="card">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="问题描述" name="first">
+          <SalesQuestion />
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import SalesQuestion from "./question";
+export default {
+  name: "thirteen",
+  components: { SalesQuestion },
+  props: {},
+  data() {
+    return {
+      options: [],
+      value: "选项1",
+      tableData: [],
+      loading: false,
+      classState: [],
+      stateflag: false,
+      activeName: "first",
+      isFirst: true,
+      labelPosition: "right",
+      formLabelAlign: {
+        name: "",
+        region: "",
+        type: "",
+      },
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+/deep/ .el-table .error-row {
+  background: #fff0f0;
+}
+/deep/ .el-table .success-row {
+  background-color: #f7fff9;
+}
+.card {
+  background-color: #eddeaa;
+  border: 1px solid #ccc; /* 可选：添加边框 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 添加边框阴影 */
+  border-radius: 8px; /* 可选：添加圆角 */
+  padding: 16px; /* 可选：添加内边距 */
+}
+.item {
+  margin-bottom: 10px;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+.main-form {
+  margin-top: 10px;
+}
+.main-button {
+  width: 100%;
+  margin-top: 10px;
+}
+.box-card {
+  padding: 0;
+}
+.single-form {
+  width: 600px;
+  top: 50%;
+  left: 50%;
+}
+.block {
+}
+</style>
